a51d3809567866e55097133c8ea1e027092aa64d
Michel Mbem <Michel.Mbem@USherbrooke.ca>
2025-03-16 19:01:27 -0400

    Added Swagger documentation and a Dockerfile

---
+++ src/main/java/org/addy/hello_boot/configuration/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package org.addy.hello_boot.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI configureOpenAPI() {
+        return new OpenAPI().info(new Info()
+                .title("Hello Boot")
+                .description("Simple Spring Boot REST API")
+                .license(getLicense())
+                .termsOfService("https://docs.github.com/site-policy/github-terms/github-terms-of-service")
+                .version("1.0.0")
+                .contact(getContact()));
+    }
+
+    private License getLicense() {
+        return new License()
+                .name("Apache-2.0")
+                .url("https://www.apache.org/licenses/LICENSE-2.0.html");
+    }
+
+    private Contact getContact() {
+        return new Contact()
+                .name("AddySoft")
+                .email("addysoft@addy.org")
+                .url("https://www.addy.org/addy-soft/");
+    }
+
+}
